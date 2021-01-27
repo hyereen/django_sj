f2376a3652516ae74f5660c68ea74076ccea351a
Hyereen Kong <reen13@ajou.ac.kr>
2021-01-27 23:16:06 +0900

    04 Framework Django 실전 - 03 View - 03 로그인 만들기

--- fc_django/fcuser/views.py
+++ fc_django/fcuser/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.views.generic.edit import FormView
-from .forms import RegisterForm
+from .forms import RegisterForm, LoginForm
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'email': request.session.get('user') })
+     
 
 
 
@@ -12,3 +13,14 @@ class RegisterView(FormView): # 폼 뷰를 상속받아서 사용
     form_class = RegisterForm
     success_url = '/' # root page로 이동 
 
+
+class LoginView(FormView):
+    template_name ='login.html'
+    form_class =LoginForm
+    success_url='/'
+
+    def form_valid(self, form):
+        # 로그인이 정상적으로 되었을 때(유효성검사 끝) 세션에 저장
+        self.request.session['user'] = form.email # 사용자가 로그인한 이메일 정보를 세션에 저장
+
+        return super().form_valid(form) # 기존의 form_valid 함수 호출 
