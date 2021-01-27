7d6cd9a4c994a92e5a27fa8d5d918181bc6c0d0d
Hyereen Kong <reen13@ajou.ac.kr>
2021-01-27 14:10:51 +0900

    04 Framework Django 실전 - 02 설정

--- fc_django/fcuser/admin.py
+++ fc_django/fcuser/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Fcuser
 
-# Register your models here.
+class FcuserAdmin(admin.ModelAdmin):
+    list_display = ('email',) # 콤마를 꼭 넣어줘야 함, 튜플로 인식시키기 위해 
+
+
+admin.site.register(Fcuser, FcuserAdmin)
--- fc_django/order/models.py
+++ fc_django/order/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Order(models.Model):
+    fcuser = models.ForeignKey('fcuser.Fcuser', on_delete=models.CASCADE, verbose_name='사용자')
+    # fcuser 앱 안에 있는 Fcuser 모델을 이용하겠다
+    # foreignKey 쓸 때는 on_delete 속성 꼭 써줘야 함, 사용자가 삭제 됐을 때 order을 어떻게 관리할지
+    # CASCADE -> 유저가 삭제되면 유저도 삭제하겠다
+    product = models.ForeignKey('product.Product', on_delete=models.CASCADE, verbose_name='상품')
+    quantity = models.IntegerField(verbose_name='수량')
+    register_date = models.DateTimeField(auto_now_add=True, verbose_name='등록날짜')
+
+    def __str__(self):
+        return str(self.fcuser) + ' ' + str(self.product)
+    class Meta:
+        db_table = 'fastcampus_order'
+        verbose_name = '주문'
+        verbose_name_plural = '주문'
